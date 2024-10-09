837f1983bd868d55a7d08bf876322d29d92770f9
DSM2 <fatec-dsm2@academico.intra>
2024-10-08 22:51:57 -0300

    adição crud feito em aula

--- index.js
+++ index.js
@@ -1,88 +1,48 @@
-const express = require("express");
+// Importando o Express com ES6 Modules
+import express from "express";
+// Iniciando o Express na variável app
 const app = express();
+// Importando os Controllers (onde estão as rotas)
+import connection from "./config/sequelize-config.js";
+import ClientesController from "./controllers/ClientesController.js";
+import ProdutosController from "./controllers/ProdutosController.js";
+import PedidosController from "./controllers/PedidosController.js";
+//permitir caputarar dados vindo de um formulario
+app.use(express.urlencoded())
+// realizando a conexão com o banco de dados
+connection.authenticate().then(()=>{
+    console.log("Conexão realizada com sucesso");
+}).catch((error)=>{
+    console.log(error)
+});
+
+connection.query(`create database if not exists loja;`).then(()=>{
+    console.log("O banco foi criado com sucesso")
+}).catch((error)=>{
+    console.log(error)
+})
 
 
-app.set("view engine", "ejs")
+// Define o EJS como Renderizador de páginas
+app.set("view engine", "ejs");
+// Define o uso da pasta "public" para uso de arquivos estáticos
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+// Definindo o uso das rotas dos Controllers
+app.use("/", ClientesController);
+app.use("/", ProdutosController);
+app.use("/", PedidosController);
+
+// ROTA PRINCIPAL
+app.get("/", function (req, res) {
   res.render("index");
 });
 
-
-app.get("/clientes", (req, res) => {
- const ListaClientes = [{
-    nome: "Éric", cpf: "123.456.789.10", endereco: "Rua dos Predios"
- }, 
- {
-    nome: "Enzo", cpf: "754.789.435.20", endereco: "Rua do Lago"
- },
- {
-    nome: "João Paulo", cpf: "654.234.890.30", endereco: "Rua da Praça"
- },
- {
-    nome: "Antonio", cpf: "342.456.765.40", endereco: "Rua do Comercio"
- }];
-    res.render("clientes", {ListaClientes:ListaClientes});
-});
-
-app.get("/produtos", (req, res) => {
-const listaProdutos = [{
-  nomeProd: 'Pagani Zonda', preco: 39.99, categoria: 'Supercarro'
-},
-{
-  nomeProd: 'Audi RS6', preco: 1200000.00, categoria: 'SuperEsportivo'
-},
-{
-  nomeProd: 'BMW M3 Touring', preco: 530000.00, categoria: 'Super carro'
-},
-{
-  nomeProd: 'Volkswagen Parati', preco: 25000.00, categoria: 'Super carro'
-}
-];
-
-  res.render("produtos", {listaProdutos: listaProdutos});
-});
-
-app.get("/pedidos", (req, res) => {
-  listaCompras = [{
-    nome: "Bugatti Veyron", preco: 16000000.00, categoria:"Supercarro"
-  },
-  {
-    nome: "Renault Sander", preco: 80000.00, categoria:"Compactos"
-  },
-  {
-    nome: "Mitsubishi Lancer Evo", preco: 200000.00, categoria:"Esportivo"
-  },
-  {
-    nome: "Mazda Rx7", preco: 100000.00, categoria:"Esportivo"
-  }
-]
-
-listaVendas = [{
-  nome: "Ferrari La Ferrari", preco: 16000000.00, categoria:"Supercarro"
-},
-{
-  nome: "Audi RSQ8", preco: 1200000.00, categoria:"SUV"
-},
-{
-  nome: "Fiat palio", preco: 29000.00, categoria:"Compactos"
-},
-{
-  nome: "BWM Isetta", preco: 60000.00, categoria:"Compactos"
-}
-]
-
-
-
-  res.render("Pedidos", {listaCompras: listaCompras, listaVendas: listaVendas});
-});
-
-//config da porta do sistema de loja
-const port = 8080;
-app.listen(8080, function (error) {
-  if (error) {
-    console.log(`O erro é: ${error}`);
+// INICIA O SERVIDOR NA PORTA 8080
+app.listen(8080, function (erro) {
+  if (erro) {
+    console.log("Ocorreu um erro!");
   } else {
-    console.log(`Conectado com sucesso. http://localhost:${port}`);
+    console.log("Servidor iniciado com sucesso!");
   }
 });
